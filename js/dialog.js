84d31fbbe3b2f14289664dc05fabf2edcea36699
Георгий Чмутин <GNChmutin.SBT@sberbank.ru>
2019-04-28 11:21:22 +0300

    сценарий регистрации + получение system_code

---
+++ js/dialog.js
@@ -0,0 +1,72 @@
+function start() {
+    fetch('/back/start', {
+        method: 'GET',
+        credentials: 'include'
+    })
+        .then(handleVoiceResponse)
+        .then(playSound)
+}
+
+function onRecordStart() {
+    startRecording();
+    $('#recordButton').hide();
+    $('#stopButton').show();
+}
+
+function onRecordedVoiceObtained(blob) {
+    var url = URL.createObjectURL(blob);
+    au = $('#audio')
+    au[0].controls = true
+    au[0].src = url
+    au.show()
+
+    fetch('/back/voice', {method: 'POST', credentials: 'include', body: blob})
+        .then(handleVoiceResponse)
+        .then(data => {
+            playSound(data)
+            fetch('/back/get_dialog', {method: 'GET', credentials: 'include'})
+                .then(response => response.json())
+                .then(addDialogMessage)
+        })
+
+}
+
+function onRecordStop() {
+    $('#recordButton').show();
+    $('#stopButton').hide();
+    stopRecording(onRecordedVoiceObtained);
+}
+
+function addDialogMessage(data) {
+    const message = document.createElement('div');
+    message.className = 'message'
+    const input_phrase = document.createElement('div');
+    input_phrase.innerText = data.input_phrase
+    input_phrase.className = 'input-phrase'
+    const text_to_show = document.createElement('div');
+    text_to_show.innerText = data.text_to_show
+    text_to_show.className = 'text-to-show'
+    message.appendChild(input_phrase)
+    message.appendChild(text_to_show)
+    $('#dialog')[0].appendChild(message)
+}
+
+function handleVoiceResponse(response) {
+    const reader = response.body.getReader();
+    // read() returns a promise that resolves
+    // when a value has been received
+    return reader
+        .read()
+        .then((result) => {
+            return result;
+        });
+}
+
+function playSound(data) {
+    const blob = new Blob([data.value], {type: 'audio/mp3'});
+    let url = URL.createObjectURL(blob);
+    console.log('url of sound to play: ' + url);
+    let sound = document.createElement('audio');
+    sound.src = url;
+    sound.play();
+}
--- js/registration.js
+++ js/registration.js
@@ -1,8 +1,47 @@
 function sendName() {
     user_name = $('#user_name').val()
-    fetch('/back/user/name', {method: 'GET'})
-        .then(resp => {console.log('Name was successfully sent')})
-        .catch(err => {console.error(err)})
+    fetch('/back/add_user/name', {
+        method: 'POST',
+        body: JSON.stringify({name: user_name}),
+        headers: {'Content-Type': 'application/json; utf-8'},
+        credentials: 'include'
+    })
+        .then(resp => {
+            console.log('Name was successfully sent')
+            $('#user_name')[0].disabled = true
+            $('#register_user_name')[0].disabled = true
+        })
+        .catch(err => {
+            console.error(err)
+        })
+}
+
+function sendControlWords() {
+    const word1 = $('#word1').val()
+    const word2 = $('#word2').val()
+    const word3 = $('#word3').val()
+    const word4 = $('#word4').val()
+    const word5 = $('#word5').val()
+    const data = {words: [word1, word2, word3, word4, word5]}
+
+    fetch('/back/add_user/words', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {'Content-Type': 'application/json'},
+        credentials: 'include'
+    })
+        .then(resp => {
+            console.log('Control words were successfully sent')
+            $('#word1')[0].disabled = true
+            $('#word2')[0].disabled = true
+            $('#word3')[0].disabled = true
+            $('#word4')[0].disabled = true
+            $('#word5')[0].disabled = true
+            $('#send_control_words')[0].disabled = true
+        })
+        .catch(err => {
+            console.error(err)
+        })
 }
 
 function onRecordStart() {
@@ -19,8 +58,23 @@ function onVoiceObtained(blob) {
     au.show()
 
     function sendWav() {
-        fetch('/back/voice', {method: 'POST', body: blob})
+        console.log('sending wav')
+        fetch('/back/add_user/voice', {method: 'POST', body: blob, credentials: 'include'})
+            .then(response => {
+                console.log('response: ', response)
+                if (response.headers && response.headers.get('Content-Type') === 'application/json') {
+                    response.json()
+                        .then(resp => {
+                            console.log('Получили системный код')
+                            $('#system_code_container').show()
+                            $('#system_code')[0].innerHTML = resp['gen_secret']
+                        })
+                } else {
+                    handleVoiceResponse(response).then(playSound)
+                }
+            })
     }
+
     btn = $('#send')
     btn.click(sendWav)
     btn.show()
@@ -32,15 +86,25 @@ function onRecordStop() {
     stopRecording(onVoiceObtained);
 }
 
-function sendControlWords() {
-    const word1 = $('#word1').val()
-    const word2 = $('#word2').val()
-    const word3 = $('#word3').val()
-    const word4 = $('#word4').val()
-    const word5 = $('#word5').val()
-    const data = {word1, word2, word3, word4, word5}
+function handleVoiceResponse(response) {
+    const reader = response.body.getReader();
+    // read() returns a promise that resolves
+    // when a value has been received
+    return reader
+        .read()
+        .then((result) => {
+            return result;
+        });
+}
 
-    fetch('/back/user/name', {method: 'POST', body: JSON.stringify(data), headers: {'Content-Type': 'application/json'}})
-        .then(resp => {console.log('Name was successfully sent')})
-        .catch(err => {console.error(err)})
+function playSound(data) {
+    const blob = new Blob([data.value], {type: 'audio/mp3'});
+    let url = URL.createObjectURL(blob);
+    console.log('url of sound to play: ' + url);
+    let sound = document.createElement('audio');
+    sound.id = "audio"
+    sound.src = url;
+    sound.play();
+    audio = document.getElementById('audio');
+    audio.parentNode.removeChild(audio);
 }
